42bff34e8a27b0932e698ab8fdeac2fc8ed3fe32
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2020-10-07 19:01:33 -0600

    modify window initialization; wayland issue will be resolved on bracket-lib end

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,12 @@ use bracket_lib::prelude::*;
 use blademaster::State;
 
 fn main() -> BError {
-    let ctx = BTermBuilder::simple(60, 30)?
-        .with_tile_dimensions(20, 20)
-        .with_fullscreen(true)
+    let ctx = BTermBuilder::simple80x50()
         .with_advanced_input(true)
+        .with_automatic_console_resize(true)
         .with_title("Blademaster")
         .build()?;
-    let gs = State::new(60, 30);
+    let gs = State::new(80, 50);
 
     main_loop(ctx, gs)
 }
